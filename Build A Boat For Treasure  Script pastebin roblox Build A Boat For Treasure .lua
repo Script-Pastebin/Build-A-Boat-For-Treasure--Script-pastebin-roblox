db9ce3c970441d63808b05b7c5c18700c16b9d1b
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-24 20:52:40 +0100

    commit zero

---
+++ Build A Boat For Treasure  Script pastebin roblox Build A Boat For Treasure .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+for i,v in pairs(game:GetService("Players").LocalPlayer.PlayerGui.BuildGui.InventoryFrame.ScrollingFrame.BlocksFrame:GetChildren()) do     if v:FindFirstChild("AmountText") then         v.AmountText.Text = 999         v.AmountText.Changed:Connect(function()             v.AmountText.Text = 999         end)     end end
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Build A Boat For Treasure INFINETY BLOCK
+						
+]]
